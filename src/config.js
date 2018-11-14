fd1052fd6d4f9a0fc0a7030a467fe3be5eb7a870
bscaspar <bradyscaspar@outlook.com>
2018-11-14 14:57:06 -0800

    configuring environments

--- src/config.js
+++ src/config.js
@@ -1,17 +1,40 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-uploads"
+    REGION: "us-west-2",
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-otu9zxjcpm01"
   },
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
+    REGION: "us-west-2",
+    URL: "https://api.bcaspar.com/dev"
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
-    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_ZZfYgPp5u",
+    APP_CLIENT_ID: "llso6vjkggac0b6t8npcieltk",
+    IDENTITY_POOL_ID: "us-west-2:d36989d8-78ae-4ea0-8f9c-22f6f835ce9e"
   }
+};
+
+const prod = {
+  s3: {
+    REGION: "us-west-2",
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-rjphcya6z7p8"
+  },
+  apiGateway: {
+    REGION: "us-west-2",
+    URL: "https://api.bcaspar.com/prod"
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_vtb1PK76v",
+    APP_CLIENT_ID: "14ctmatbqldnkgtt1vgnkbieqj",
+    IDENTITY_POOL_ID: "us-west-2:4181f95a-8269-4f63-a099-d11333fa540c"
+  }
+};
+
+const config = process.env.REACT_APP_STAGE === "prod" ? prod : dev;
+
+export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
